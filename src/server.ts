864d3f3c8d16dbdc071031e51e3b330e162cfe14
rodrigo <jrodrigo0592@gmail.com>
2022-09-05 15:25:33 -0300

    🧰 Updating the server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
 
-app.use("/", (request, response) => {
-  return response.json({
-    message: "Hello World!",
-  });
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running at port 3000"));
