14025df87710e27d10edf1eaa14b6af622f179ed
rodrigo <jrodrigo0592@gmail.com>
2022-09-06 12:05:01 -0300

    🔧 Updating Routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
-import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 
 routes.post("/client/", createClientController.handle);
-routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/deliveryman/", createDeliverymanController.handle);
+
+routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle
+);
 
 export { routes };
