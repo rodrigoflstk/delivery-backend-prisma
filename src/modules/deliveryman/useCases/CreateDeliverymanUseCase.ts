8f309da992c270a29e83172eb2a6c1954357668a
rodrigo <jrodrigo0592@gmail.com>
2022-09-06 12:04:11 -0300

    ➕ Created Deliveryman Module

---
+++ src/modules/deliveryman/useCases/CreateDeliverymanUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../../database/PrismaClient";
+import * as bcrypt from "bcrypt";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+class CreateDeliverymanUseCase {
+  async execute({ password, username }: ICreateDeliveryman) {
+    const deliverymanExists = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (deliverymanExists) {
+      throw new Error("Deliveryman Already Exists");
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    return deliveryman;
+  }
+}
+
+export { CreateDeliverymanUseCase };
