7e3d85c009cf78e056dccea604fd20c4bae8c036
rodrigo <jrodrigo0592@gmail.com>
2022-09-07 16:55:39 -0300

    🚧 Routes updated

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,38 @@
 import { Router } from "express";
+import { middlewareToValidateJwt } from "./middlewares/ensureAuthenticateClient";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/CreateDeliverymanController";
+
+// ======================================================================================== Instanciando os Controllers
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+
+const createDeliveryController = new CreateDeliveryController();
+
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController =
   new AuthenticateDeliverymanController();
 
-routes.post("/client/", createClientController.handle);
-routes.post("/deliveryman/", createDeliverymanController.handle);
+// ======================================================================================== configurando os endpoints
 
-routes.post("/client/authenticate", authenticateClientController.handle);
+routes.post("/client/signup", createClientController.handle);
+routes.post("/deliveryman/signup", createDeliverymanController.handle);
+
+routes.post("/client/signin", authenticateClientController.handle);
+routes.post("/deliveryman/signin", authenticateDeliverymanController.handle);
+
 routes.post(
-  "/deliveryman/authenticate",
-  authenticateDeliverymanController.handle
+  "/delivery/register",
+  middlewareToValidateJwt,
+  createDeliveryController.handle
 );
+// ========================================================================================
 
 export { routes };
