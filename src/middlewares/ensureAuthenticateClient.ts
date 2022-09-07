866037f6f330d40e113829e148531fce41fa4351
rodrigo <jrodrigo0592@gmail.com>
2022-09-07 16:54:02 -0300

    Adding new middleware

---
+++ src/middlewares/ensureAuthenticateClient.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+  sub: string;
+}
+
+const middlewareToValidateJwt = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  const token = request.headers.authorization;
+  const secretKey = "c4e70ca5e4aef7ce6dcb11e2dab50966";
+
+  if (!token) {
+    return response.status(401).json({
+      message: "Token is missing!!",
+    });
+  }
+
+  const [, tokenContent] = token.split(" ");
+
+  try {
+    const { sub } = verify(tokenContent, secretKey) as IPayload;
+
+    request.client_id = sub;
+
+    return next();
+  } catch (error) {
+    return response.status(401).json({
+      message: "Invalid token !!",
+    });
+  }
+};
+
+export { middlewareToValidateJwt };
