651f1883129aa16f94adb0c1e692edf8fc46c968
rodrigo <jrodrigo0592@gmail.com>
2022-09-08 14:15:11 -0300

    🚧 Database Changes and Updates

--- prisma/migrations/20220906153649_alter_table_deliveries/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "deliveries" DROP CONSTRAINT "deliveries_delivery_id_fkey";
-
--- AlterTable
-ALTER TABLE "deliveries" ALTER COLUMN "delivery_id" DROP NOT NULL,
-ALTER COLUMN "end_at" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_delivery_id_fkey" FOREIGN KEY ("delivery_id") REFERENCES "deliveryman"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ prisma/migrations/20220908133606_create_tables/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "deliveryman" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "deliveryman_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "client" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "deliveries" (
+    "id" TEXT NOT NULL,
+    "item_name" TEXT NOT NULL,
+    "client_id" TEXT NOT NULL,
+    "deliveryman_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end_at" TIMESTAMP(3),
+
+    CONSTRAINT "deliveries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "deliveryman_username_key" ON "deliveryman"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "client_username_key" ON "client"("username");
+
+-- AddForeignKey
+ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "deliveries" ADD CONSTRAINT "deliveries_deliveryman_id_fkey" FOREIGN KEY ("deliveryman_id") REFERENCES "deliveryman"("id") ON DELETE SET NULL ON UPDATE CASCADE;
