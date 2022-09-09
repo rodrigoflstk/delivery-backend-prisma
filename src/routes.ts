9cf90751277b8ab6873ed1cb3310755a246ff648
rodrigo <jrodrigo0592@gmail.com>
2022-09-09 13:04:11 -0300

    ➕ Adding new Routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,30 +5,47 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/client/useCases/createClient/CreateClientController";
+import { FindAllDeliveriesController } from "./modules/client/useCases/delivery/FindAllDeliveriesController";
+import { AddDeliverymanController } from "./modules/deliveries/useCases/addDeliveryman/AddDeliverymanController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { FindOpenedDeliveryController } from "./modules/deliveries/useCases/findOpenedDelivery/FindOpenedDeliveryController";
-import { CreateDeliverymanController } from "./modules/deliveryman/useCases/CreateDeliverymanController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { FindAllDeliverymanDeliveriesController } from "./modules/deliveryman/useCases/delivery/FindAllDeliveriesController";
 
 const routes = Router();
 
-const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
+const addDeliverymanController = new AddDeliverymanController();
+const findAllDeliverymanDeliveriesController =
+  new FindAllDeliverymanDeliveriesController();
 
 const createDeliveryController = new CreateDeliveryController();
 const findOpenedDeliveryController = new FindOpenedDeliveryController();
 
+const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
-const authenticateDeliverymanController =
-  new AuthenticateDeliverymanController();
+const findAllDeliveriesController = new FindAllDeliveriesController();
 
 routes.post("/client/signup", createClientController.handle);
-routes.post("/deliveryman/signup", createDeliverymanController.handle);
-
 routes.post("/client/signin", authenticateClientController.handle);
+routes.get(
+  "/client/deliveries",
+  middlewareToValidateClient,
+  findAllDeliveriesController.handle
+);
+
+routes.post("/deliveryman/signup", createDeliverymanController.handle);
 routes.post("/deliveryman/signin", authenticateDeliverymanController.handle);
+routes.get(
+  "/deliveryman/deliveries",
+  middlewareToValidateDeliveryman,
+  findAllDeliverymanDeliveriesController.handle
+);
 
 routes.get(
-  "/opened/delivery",
+  "/delivery/opened",
   middlewareToValidateDeliveryman,
   findOpenedDeliveryController.handle
 );
@@ -36,6 +53,11 @@ routes.post(
   "/delivery/register",
   middlewareToValidateClient,
   createDeliveryController.handle
+);
+routes.put(
+  "/delivery/addDeliveryman/:id",
+  middlewareToValidateDeliveryman,
+  addDeliverymanController.handle
 );
 
 export { routes };
