83ab93f11dbab66c9ffffcf43e048c008b954a94
rodrigo <jrodrigo0592@gmail.com>
2022-09-23 17:38:26 -0300

    🔧 Updating Deliveries Modules

--- src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.ts
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryController.ts
@@ -3,13 +3,17 @@ import { CreateDeliveryUseCase } from "./CreateDeliveryUseCase";
 
 class CreateDeliveryController {
   async handle(request: Request, response: Response) {
-    const { item_name } = request.body;
+    const { item_name, description, pickup_address, delivery_address } =
+      request.body;
     const { client_id } = request;
     const createDeliveryUseCase = new CreateDeliveryUseCase();
 
     const delivery = await createDeliveryUseCase.execute({
       client_id,
       item_name,
+      description,
+      pickup_address,
+      delivery_address,
     });
 
     return response.json(delivery);
--- src/modules/deliveries/useCases/createDelivery/CreateDeliveryUseCase.ts
+++ src/modules/deliveries/useCases/createDelivery/CreateDeliveryUseCase.ts
@@ -3,14 +3,26 @@ import { prisma } from "../../../../database/PrismaClient";
 interface ICreateDelivery {
   item_name: string;
   client_id: string;
+  pickup_address: string;
+  delivery_address: string;
+  description: string;
 }
 
 class CreateDeliveryUseCase {
-  async execute({ item_name, client_id }: ICreateDelivery) {
+  async execute({
+    item_name,
+    client_id,
+    delivery_address,
+    pickup_address,
+    description,
+  }: ICreateDelivery) {
     const delivery = await prisma.deliveries.create({
       data: {
         item_name,
         client_id,
+        pickup_address,
+        delivery_address,
+        description,
       },
     });
 
